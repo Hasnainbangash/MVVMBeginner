21b4f689417b46a37f84d42f4073c3e9d803f0e4
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-17 14:50:03 +0500

    Initial coomit content view littl ebit design work done

--- MVVMBeginner/ContentView.swift
+++ MVVMBeginner/ContentView.swift
@@ -7,17 +7,56 @@
 
 import SwiftUI
 
+// MARK: - ITEM MODEL
+
+struct Item: Identifiable {
+    var id: UUID
+    var name: String
+}
+
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    @State var items: [Item] = [
+        Item(id: UUID(), name: "first"),
+        Item(id: UUID(), name: "second"),
+        Item(id: UUID(), name: "third")
+    ]
+    
+    // MARK: - BODY
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            List {
+                ForEach(items) { item in
+                    Text(item.name)
+                } //: LOOP
+                .onDelete { indexSet in
+                    for index in indexSet {
+                        items.remove(at: index)
+                    }
+                }
+                .onMove { indices, newOffset in
+                    items.move(fromOffsets: indices, toOffset: newOffset)
+                }
+            } //: LIST
+            .navigationBarTitle("Todo's", displayMode: .large)
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    EditButton()
+                    
+                    Button(action: {
+                        items.append(Item(id: UUID(), name: "newly added"))
+                    }, label: {
+                        Image(systemName: "plus")
+                    }) //: BUTTON
+                }
+            } //: TOOLBAR
+        } //: NAVIGATION
     }
 }
+
+// MARK: - PREVIEW
 
 #Preview {
     ContentView()
