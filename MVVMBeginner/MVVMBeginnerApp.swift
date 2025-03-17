e0500b4da06f871570d95b092945290166f35899
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-17 14:26:27 +0500

    Initial Commit

---
+++ MVVMBeginner/MVVMBeginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMBeginnerApp.swift
+//  MVVMBeginner
+//
+//  Created by Elexoft on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MVVMBeginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
