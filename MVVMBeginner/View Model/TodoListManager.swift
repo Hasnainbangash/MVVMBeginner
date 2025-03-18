4af3ef7d782822ca4b76d5d1763bc941d00b5a68
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-18 10:56:01 +0500

    in view model a  new file is genrated todolist mananger and move the items array array there

---
+++ MVVMBeginner/View Model/TodoListManager.swift
@@ -0,0 +1,20 @@
+//
+//  TodoListManager.swift
+//  MVVMBeginner
+//
+//  Created by Elexoft on 18/03/2025.
+//
+
+import Foundation
+
+class TodoListManager: ObservableObject {
+    @Published var items: [Item] = [
+        Item(id: UUID(), name: "first"),
+        Item(id: UUID(), name: "second"),
+        Item(id: UUID(), name: "third")
+    ]
+    
+    init() {
+        //get my data and set to my items array
+    }
+}
